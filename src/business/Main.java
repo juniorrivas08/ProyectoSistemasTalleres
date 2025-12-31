78b22fc579c157c4b0571305158045eae6ce7a68
juniorivas08 <junior.rivas.parrales@est.una.ac.cr>
2025-12-31 13:38:55 -0600

    Se agrega el mensaje en el main

--- src/business/Main.java
+++ src/business/Main.java
@@ -10,6 +10,8 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
+			
+			System.out.println("Esto es un mensaje nuevo");
 			BorderPane root = new BorderPane();
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
