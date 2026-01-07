01307b0f9c8ae2b040ebcfde96ff5b38818c462a
juniorrivas08 <junior.rivas.parrales@est.una.ac.cr>
2026-01-06 21:46:25 -0600

    Revert "Revert "Agregue CRUD vehiculo, cambie JsonUtils y clase mechanic""
    This reverts commit a24dda58e2f15d02f4df69706ed8eb3a8aed6a13.

--- src/domain/Mechanic.java
+++ src/domain/Mechanic.java
@@ -1,16 +1,31 @@
 package domain;
 
-public class Mechanic extends Person{
+public class Mechanic {
 
+	private String id;
 	private String specialty;
+	private String nameMechanic;
+	private String mail;
+	private String phoneMechanic;
+	
 
 	public Mechanic() {
 		
 	}
-
-	public Mechanic(String id, String name, String phone, String mail, String specialty) {
-		super(id, name, phone, mail);
+	
+	public Mechanic(String specialty, String nameMechanic, String mail, String phoneMechanic) {
 		this.specialty = specialty;
+		this.nameMechanic = nameMechanic;
+		this.mail = mail;
+		this.phoneMechanic = phoneMechanic;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
 	}
 
 	public String getSpecialty() {
@@ -20,10 +35,38 @@ public class Mechanic extends Person{
 	public void setSpecialty(String specialty) {
 		this.specialty = specialty;
 	}
-	
-	public String toString() {
-		return super.toString()+specialty;
+
+	public String getNameMechanic() {
+		return nameMechanic;
 	}
+
+	public void setNameMechanic(String nameMechanic) {
+		this.nameMechanic = nameMechanic;
+	}
+
+	public String getMail() {
+		return mail;
+	}
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+
+	public String getPhoneMechanic() {
+		return phoneMechanic;
+	}
+
+	public void setPhoneMechanic(String phoneMechanic) {
+		this.phoneMechanic = phoneMechanic;
+	}
+
+	@Override
+	public String toString() {
+		return "Mechanic [id=" + id + ", specialty=" + specialty + ", nameMechanic=" + nameMechanic + ", mail=" + mail
+				+ ", phoneMechanic=" + phoneMechanic + "]";
+	}
+	
+	
 	
 	
 }
