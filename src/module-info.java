b4498682d555db13ad068546d50899b799baee66
juniorrivas08 <junior.rivas.parrales@est.una.ac.cr>
2026-01-06 21:46:25 -0600

    Revert "Revert "Agregue CRUD vehiculo, cambie JsonUtils y clase mechanic""
    This reverts commit a24dda58e2f15d02f4df69706ed8eb3a8aed6a13.

--- src/module-info.java
+++ src/module-info.java
@@ -2,7 +2,8 @@ module ProyectoSistemasTalleres {
 	requires javafx.controls;
 	requires com.fasterxml.jackson.datatype.jsr310;
 	requires com.fasterxml.jackson.databind;
+	requires javafx.fxml;
 	
-	
+	opens domain;
 	opens business to javafx.graphics, javafx.fxml;
 }
